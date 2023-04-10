b90b830ae73a2a38db1cd8c0247c04dde46db09c
mmartin4972 <mmartin4972@gmail.com>
2023-04-10 16:58:44 -0400

    Made slight modification to common

--- common.py
+++ common.py
@@ -9,9 +9,9 @@ def hello():
 def get_baseline_data(file: str) :
     nba_initial = pd.read_csv(file, index_col=[0])
     nba = nba_initial.dropna()
-    nba['inflationAdjSalary'] = nba['inflationAdjSalary'].str.replace('$', '')
-    nba['inflationAdjSalary'] = nba['inflationAdjSalary'].str.replace(',', '')
-    nba['inflationAdjSalary'] = nba['inflationAdjSalary'].astype(int)
+    # nba['inflationAdjSalary'] = nba['inflationAdjSalary'].str.replace('$', '')
+    # nba['inflationAdjSalary'] = nba['inflationAdjSalary'].str.replace(',', '')
+    # nba['inflationAdjSalary'] = nba['inflationAdjSalary'].astype(int)
     nba['inflationAdjSalary_log'] = nba['inflationAdjSalary'].apply(lambda x: np.log(x))
     return nba
 
