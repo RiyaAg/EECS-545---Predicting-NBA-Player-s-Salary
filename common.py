0e7b59e94b4f3fa6a66ca710473aa1b99080c43e
mmartin4972 <mmartin4972@gmail.com>
2023-04-10 08:18:18 -0400

    Synced changes

--- common.py
+++ common.py
@@ -8,6 +8,7 @@ def get_baseline_data(file: str) :
     nba['inflationAdjSalary'] = nba['inflationAdjSalary'].str.replace(',', '')
     nba['inflationAdjSalary'] = nba['inflationAdjSalary'].astype(int)
     nba['inflationAdjSalary_log'] = nba['inflationAdjSalary'].apply(lambda x: np.log(x))
-    X = nba[['MP', 'PTS', 'Age', 'games_started', 'PER', 'AST', 'STL', 'TRB', '3P']]
-    y = nba[['inflationAdjSalary_log']]
     return nba
+
+# May want to consider averaging the parameters of two different models where one is tested on data that incorporates a certain statistic while another includes data which doesn't incorporate that statistic
+# can also experiment with the best method for handling these nan values https://www.forbes.com/sites/markdeeks/2022/07/01/a-complete-history-of-nba-luxury-tax-payments-20012022/?sh=75991719432f
