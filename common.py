a50ca1c073046e99124625fb68e8e4f5bee63efd
mmartin4972 <mmartin4972@gmail.com>
2023-04-18 22:44:29 -0400

    Successfully implemented model for predicting base states from external and projected salary. Need to play with it more and derive more interesting numbers

--- common.py
+++ common.py
@@ -1,15 +1,24 @@
 import pickle
 import os
 from pathlib import Path
-from models.preprocessing import get_baseline_data
 import pandas as pd
 import numpy as np
+import math
+
+# Constants
+NEXT_Y_SAL = 'next_year_salary'
+INF_ADJ_SAL = 'inflationAdjSalary'
+SZN_START_Y = 'seasonStartYear'
+P_NAME = 'playerName'
+NN_FILENAME = 'neural_network.model'
+RF_FILENAME = 'random_forest.model'
+EXT_NEXT_Y_FILENAME = 'external_cleaned_next_year_salary.csv'
 
 # Returns the salary class of the given salaryl
 def get_salary_class(salary: float, max: float, min:float) :
     num_classes = 10
-    (max-min)/num_classes
-    return 0
+    interval = (max-min)/num_classes
+    return math.floor((salary - min)/interval)
 
 def get_baseline_data(file: str) :
     nba_initial = pd.read_csv(file, index_col=[0])
@@ -20,18 +29,30 @@ def get_baseline_data(file: str) :
     nba['inflationAdjSalary_log'] = nba['inflationAdjSalary'].apply(lambda x: np.log(x))
     return nba
 
-NN_FILENAME = 'neural_network.model'
-RF_FILENAME = 'random_forest.model'
 def get_nn() :
     p = Path(os.path.abspath(__file__)).parent / ('models/saved_models/' + NN_FILENAME)
     return pickle.load(open(p, 'rb'))
-    
-def get_nn_features() :
+
+def get_base_features() :
     return ['MP', 'PTS', 'Age', 'games', 'games_started', 'PER', 'FTr', 'AST', 'STL', 'TRB', 'FT', '3P', 'FG']
-    
+
+def get_extern_features() :
+    return get_base_features() + ['height', 'weight','all_star_total', 'all_star_enc', 'all_nba_enc', 'all_nba_total','draft_pick', 'champion', 'conference_champ', 
+       'mvp', 'mvp_rank','mvp_total', 'player_week_enc', 'player_week_total', 'dpoy','dpoy_rank', 'dpoy_total']
+
 def get_rf() :
     p = Path(os.path.abspath(__file__)).parent / ('models/saved_models/' + RF_FILENAME)
     return pickle.load(open(p, 'rb'))
 
-# May want to consider averaging the parameters of two different models where one is tested on data that incorporates a certain statistic while another includes data which doesn't incorporate that statistic
-# can also experiment with the best method for handling these nan values https://www.forbes.com/sites/markdeeks/2022/07/01/a-complete-history-of-nba-luxury-tax-payments-20012022/?sh=75991719432f
+def add_log_y_values(dataset: pd.DataFrame):
+    dataset['inflationAdjSalary_log'] = dataset['inflationAdjSalary'].apply(lambda x: np.log(x))
+    return dataset
+
+def get_next_year_external_data() :
+    p = Path(os.path.abspath(__file__)).parent / ('data/cleaned_data/' + EXT_NEXT_Y_FILENAME)
+    nba = pd.read_csv(p)
+    nba = add_log_y_values(nba)
+    nba = nba.drop(columns=['Unnamed: 0.2', 'Unnamed: 0', 'Unnamed: 0.1', 'college','archetype','all_star','all_nba', 'position','team'])
+    nba = nba.dropna()
+    nba = nba.loc[:, (nba != 0).any(axis=0)]
+    return nba
---
+++ data/add_next_salary.py
@@ -0,0 +1,46 @@
+import pandas as pd
+
+# Give yourself access to common
+import os
+import sys
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+from common import NEXT_Y_SAL, EXT_NEXT_Y_FILENAME, SZN_START_Y, P_NAME, INF_ADJ_SAL
+
+def add_next_year_salary(nba_cleaned: pd.DataFrame):
+    # nba_cleaned['prevYearSalary'] = np.nan
+    name_index = nba_cleaned.columns.get_loc(P_NAME)
+    
+    # print(nba_cleaned)
+    for start_year in range(1996, 2017):
+        next_year = nba_cleaned[nba_cleaned[SZN_START_Y] == start_year + 1]
+        cur_year = nba_cleaned[nba_cleaned[SZN_START_Y] == start_year]
+        # print(cur_year)
+        for row in cur_year.values:
+            name = row[name_index]
+            next_year_val = next_year[next_year[P_NAME] == name]
+            # print(prev_year_val)
+            if not next_year_val.empty:
+                next_salary = next_year_val[INF_ADJ_SAL].values[0]
+                # print(log_salary)
+                nba_cleaned.loc[(nba_cleaned[SZN_START_Y] == start_year) & (nba_cleaned[P_NAME] == name), NEXT_Y_SAL] = next_salary
+                # print(prev_year_val['inflationAdjSalary'])
+                # print(nba_cleaned.loc[(nba_cleaned[SZN_START_Y] == start_year) & (nba_cleaned[P_NAME] == name)])
+                # cur_year['prevYearSalaryLog'] = np.log(prev_year_val['inflationAdjSalary'])
+            # print(nba_cleaned[nba_cleaned[SZN_START_Y] == start_year])
+            
+            # break
+            # row['prevYearSalary'] = 
+        # print(nba_cleaned[nba_cleaned[SZN_START_Y] == start_year])
+        # break
+    nba_cleaned = nba_cleaned[nba_cleaned[SZN_START_Y] != 1996]
+    
+    return nba_cleaned
+
+if __name__ == "__main__":
+    nba_cleaned = pd.read_csv('cleaned_data/external_cleaned.csv')
+    nba_cleaned = add_next_year_salary(nba_cleaned)
+    
+    
+    nba_cleaned.to_csv('cleaned_data/' + EXT_NEXT_Y_FILENAME)
